519f9505291340a9d3098e737a16c0910ffe0fed
Barterr <bartgb00@gmail.com>
2017-02-15 12:49:51 -0200

    darksky api test

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global $, jQuery */
 
-function getWeather(data) {
-  "use strict";  
-  data["q"] = "New Hampshire";
-  data["APPID"] = "805418e886455718d75d86ba7717ac34";
-  // data["client_id"] = "9TWAIhUfekaWw4KFvByYq";
-  // data["client_secret"] = "1P5Cp3eLLlejzCRqS3FGJdle6QfGplR3mnlnrG6i";
-  // data["query"] = "name:Florianopolis";
-  // data["query"] = "name:Florianopolis";
+function getWeather() {
+  "use strict";
+  // console.log(lat);
+  // console.log(lon);
+  // console.log('https://api.darksky.net/forecast/a29dc7cba1edb350df6260b72ce042c9/'+lat+','+lon);
   $.ajax({
-    url: '//api.openweathermap.org/data/2.5/weather?',
-    // url: '//api.aerisapi.com/places/search?',
-    // type: 'GET',
-    data: data,
+    url: '//api.darksky.net/forecast/a29dc7cba1edb350df6260b72ce042c9/-27.595377799999998,-48.548049899999995',
+    type: 'GET',
     dataType: 'json',
     success: function(data) {
       console.log(JSON.stringify(data));
 
       // $('#quote').html(quote);
-      // $('#author').html("- " + author);
       // $('#tweet-quote').attr("href", 'https://twitter.com/intent/tweet?text=' + $('#quote').html() + "-" + author);
     },
     error: function(err) { 
       console.log(JSON.stringify(err));
+    },
+    complete: function(data) {
+      console.log(JSON.stringify(data.responseText));
     },
     beforeSend: function(xhr) {
       
@@ -31,16 +28,36 @@ function getWeather(data) {
   });
 };
 
+function codeAddress(location) {
+  geocoder = new google.maps.Geocoder();
+  geocoder.geocode( { 'address': location}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var data = {};
+      data["lat"] = results[0].geometry.location.lat();
+      data["lon"] = results[0].geometry.location.lng();
+      return data;
+
+      // console.log(results[0].geometry.location.lat());
+      // console.log(results[0].geometry.location.lng());
+    } 
+    else {
+      console.log("Geocode was not successful for the following reason: " + status);
+      return false;
+    }
+  });
+}
+
 $(document).ready(function () {
   "use strict";
   // if (navigator.geolocation) {
   //   navigator.geolocation.getCurrentPosition(function(position) {
   //     var data = {};
-  //     // data["lat"] = position.coords.latitude;
-  //     // data["lon"] = position.coords.longitude;
-  //     getWeather(data);
+  //     data["lat"] = position.coords.latitude;
+  //     data["lon"] = position.coords.longitude;
+  //     console.log(JSON.stringify(data));
+  //     getWeather(data.lat, data.lon);
   //   });
   // }
-  var data = {};
-  getWeather(data);
+  // console.log(JSON.stringify(codeAddress("Dubai")));
+  getWeather();
 });
